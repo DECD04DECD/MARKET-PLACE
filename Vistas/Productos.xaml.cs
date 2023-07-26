edf8fc9b98cb1ac8a958f181905126f6953704b6
zury <zuryguerrero85@gmail.com>
2023-07-26 00:53:01 -0600

    ultimos del 25

--- Vistas/Productos.xaml.cs
+++ Vistas/Productos.xaml.cs
@@ -59,9 +59,26 @@ namespace MARKET_PLACE.Vistas
             DataContext = new Vendedor();
         }
 
+        private void BtnMicuenta_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            MainWindow m = new MainWindow();
+            m.Show();
+
+            Close();
+        }
+
         private void BtnAyuda_Click(object sender, RoutedEventArgs e)
         {
-            DataContext = new AyudaCliente();
+            DataContext = new AyudaC();
+        }
+
+      
+
+        private void MenuP(object sender, RoutedEventArgs e)
+        {
+            DataContext = new Productos();
         }
     }
 }
